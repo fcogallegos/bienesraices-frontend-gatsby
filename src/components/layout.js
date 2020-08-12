db0dac643f535e4d73bbd2ed849793c8cbdced65
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-11 18:53:01 -0600

    adding React Helmet for add stylesheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 const Layout = ({ children }) => {
     return ( 
         <>
+        <Helmet>
+            <title>Bienes Raices Gatsby</title>
+            <meta name="description" content="Web site of bienes raices on Gatsby" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        </Helmet>
         { children }
         </>
      );
